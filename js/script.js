c182df5010ac5b0614613451d8a0273ae95b7988
Nata Malinova <rawr@raery.ru>
2021-12-08 11:48:24 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -43,7 +43,7 @@ $(document).ready(function () {
                 }
             },
             {
-                breakpoint: 480,
+                breakpoint: 767,
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1,
