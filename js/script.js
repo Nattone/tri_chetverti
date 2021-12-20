c93de1fb31a81aa704fa40d3e2900d193fce7071
Nata Malinova <rawr@raery.ru>
2021-12-20 11:26:29 +0700

    btns and js

--- js/script.js
+++ js/script.js
@@ -1,86 +1,95 @@
 $(document).ready(function () {
+  //play on video
+  $(document).on("click", ".video-wrapper", function () {
+    const video = $(this).children("video")[0];
+    if ($(this).hasClass("play")) {
+      video.pause();
+    } else {
+      video.play();
+    }
+    $(this).toggleClass("play");
+  });
 
-    //play on video
-    $(document).on('click', '.video-wrapper', function () {
-        const video = $(this).children('video')[0];
-        if ($(this).hasClass('play')) {
-            video.pause();
-        } else {
-            video.play();
-        }
-        $(this).toggleClass('play');
-    });
+  // slick slider
+  $(".about-us .slider").slick({
+    centerMode: true,
+    centerPadding: "60px",
+    slidesToShow: 1,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          centerMode: false,
+          centerPadding: "40px",
+        },
+      },
+    ],
+  });
 
+  $(".results .slider").slick({
+    centerMode: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
 
-    // slick slider 
-    $('.about-us .slider').slick({
-        centerMode: true,
-        centerPadding: '60px',
-        slidesToShow: 1,
-        responsive: [
-            {
-                breakpoint: 767,
-                settings: {
-                    centerMode: false,
-                    centerPadding: '40px',
-                }
-            }
-        ]
+  $(".teachers .slider").slick({
+    centerMode: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
 
-    });
+  // open mobile menu
+  $(document).on("click", ".menu-btn", function () {
+    $(".header-wrapper").toggleClass("open");
+  });
 
-    $('.results .slider').slick({
-        centerMode: true,
-        centerPadding: '60px',
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        responsive: [
-            {
-                breakpoint: 991,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                }
-            },
-            {
-                breakpoint: 767,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                }
-            }
-        ]
-    });
+  // input mask
+  $('input[type="tel"]').inputmask({
+    mask: "+7 (999) 999-9999",
+    placeholder: "x",
+  });
 
-    $('.teachers .slider').slick({
-        centerMode: true,
-        centerPadding: '60px',
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        responsive: [
-            {
-                breakpoint: 991,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                }
-            },
-            {
-                breakpoint: 767,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                }
-            }
-        ]
-    });
+  // open popup
+  $(document).on("click", ".btn-popup", function () {
+    $(".popup").addClass("open");
+  });
 
-    // open mobile menu
-    $(document).on('click', '.menu-btn', function () {
-        $('.header-wrapper').toggleClass('open');
-    });
-
-    // input mask
-    $('input[type="tel"]').inputmask({ "mask": "+7 (999) 999-9999", "placeholder": "x" })
-
+  // close popup
+  $(document).on("click", ".btn-close", function () {
+    $(".popup").removeClass("open");
+  });
 });
